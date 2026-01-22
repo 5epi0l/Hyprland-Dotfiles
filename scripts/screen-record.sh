ecd60d39e7fa79f138f06dac8f62110db703d08d
subh <subh@example.com>
2026-01-22 23:25:31 +0530

    fixed a minor problem in the recording script

--- scripts/screen-record.sh
+++ scripts/screen-record.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+!/bin/bash
 
 DISPLAY="$(hyprctl monitors | head -n 1 | cut -d ' ' -f 2)"
 
 notify-send -u normal -i video-display "Recording Screen" "Display: $DISPLAY"
 
-wf-recorder -Dyaf ~/Videos/recordings/video.mkv
+wf-recorder -a -Dyf ~/Videos/recordings/video.mkv
 
