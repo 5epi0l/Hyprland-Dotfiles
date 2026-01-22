e648c437574f6b732194da95bccaa6b519a79749
subh <subh@example.com>
2026-01-22 22:45:43 +0530

    added screen-recording script

---
+++ scripts/screen-record-stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+killall wf-recorder
+
+notify-send -u normal -i media-record "Recording Stopped" "Saving recording to: /home/subh/Videos/recordings/video.mkv"
---
+++ scripts/screen-record.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DISPLAY="$(hyprctl monitors | head -n 1 | cut -d ' ' -f 2)"
+
+notify-send -u normal -i video-display "Recording Screen" "Display: $DISPLAY"
+
+wf-recorder -Dyf ~/Videos/recordings/video.mkv
+
